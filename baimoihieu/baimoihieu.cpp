85aef604161585dc664cad8be7e4ae212e6a6f5c
ADMIN <ADMIN@LAB1-MAY07>
2024-02-26 08:28:55 +0700

    Add project files.

---
+++ baimoihieu/baimoihieu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, hieu;
+	cout << "chuong trinh tinh tong\n";
+	cout << "nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	hieu = so1 - so2;
+	cout << so1 << "-" << so2 << "=" << hieu << endl;
+	return 0;
+}
